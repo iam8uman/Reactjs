0acf18ff1cc98d98c8e9a7be0647b38973762ff7
iam8uman <me8848you@gmail.com>
2023-06-27 08:18:15 +0545

    js array and switch

---
+++ 30-days react/day-01/multiple.js
@@ -0,0 +1,9 @@
+let arr=['apple','ball','cat','d0g']
+let Num=['0','3','4','5']
+
+let both=[arr,Num]
+
+
+
+alert("Here is array inside another array ")
+alert([both])
---
+++ 30-days react/day-01/switch.js
@@ -0,0 +1,16 @@
+
+let case='3'
+
+switch(case){
+    case 1:
+        console.log("case first")
+        break
+    case 2:
+        console.log("case two")
+        break
+    case 3:
+        default:
+
+    case 4:
+        exit();
+}
