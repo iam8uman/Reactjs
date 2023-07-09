e93adcf067a387deb39d40bf20a0cc97ee940280
iamsuman <suman.191544@ncit.edu.np>
2023-07-09 19:29:12 +0545

    Every sec count up useEffect in function components

--- 30-days react/day7/src/App.jsx
+++ 30-days react/day7/src/App.jsx
@@ -1,10 +1,10 @@
-import everySecCount from "./components/everySecCount"
+import EverySecCount from "./components/EverySecCount"
 
 
 const App = () => {
   return (
     <div className='p-2 m-4 font-mono bg-purple-500 text-3xl'>
-      <everySecCount/>
+      <EverySecCount/>
     </div>
   )
 }
--- 30-days react/day7/src/components/everySecCount.jsx
+++ 30-days react/day7/src/components/everySecCount.jsx
@@ -1,27 +1,58 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-const everySecCount = () => {
-  const [count, setCount] = useState(0);
+// const EverySecCount = () => {
+//   const [count, setCount] = useState(0);
 
-  const Increase = () => {
-    setCount(count + 1);
-  };
+//   const Increase = () => {
+//     setCount(count + 1);
+//   };
 
-  useEffect(() => {
-    const interval = setInterval(Increase, 1000);
+//   useEffect(() => {
+//     const interval = setInterval(Increase, 1000);
+//     return()=>{
+//         clearInterval(interval)
+//     }
+//   }, [count]);
+
+//   return (
+//     <div>
+//       <div className="button p-4 m-4 text-yellow-400 text-3xl">
+//         {" "}
+//         Count up!----:   {count}{"     "}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default EverySecCount;
+
+
+
+// iamtrying to do same things without watching videos 
+
+import { useEffect, useState } from 'react'
+
+const EverySecCount = () => {
+  const [count,setCount]=useState(0)
+
+  const Increase=()=>{
+    setCount(count+1);
+  }
+
+  useEffect(()=>{
+    const interval=setInterval(Increase,1000)
     return()=>{
-        clearInterval(interval)
+      clearInterval(interval)
     }
-  }, []);
-
+  
+  },[count])
   return (
-    <div>
-      <div className="button p-4 m-4 text-yellow-400 text-3xl">
-        {" "}
-        Count up!{count}{" "}
-      </div>
-    </div>
-  );
-};
 
-export default everySecCount;
+
+    <div>
+      Count Increase Every Sec---: {count}
+    </div>
+  )
+}
+
+export default EverySecCount
