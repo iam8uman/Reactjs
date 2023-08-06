bb8a263eb593b28a8c315f51e10b5880f6485cd7
iamsuman <suman.191544@ncit.edu.np>
2023-08-06 23:19:56 +0545

    Setup almost completed

---
+++ 30-days react/PortFolio/src/hooks/useMediaQuery.jsx
@@ -0,0 +1,20 @@
+// for media query to make responsive in all devices
+
+import { useState, useEffect } from "react";
+
+const useMediaQuery=(query)=>{
+    const [matches,setMatches]=useState(false)
+
+    useEffect(()=>{
+        const media=window.matchMedia(query); //window.matchMedia vanne kura media query jo css ko
+        if(media.matches!==matches) //media ko matches mathi ko matches i.e useState wala snga 
+        setMatches(media.matches)
+
+        const listener= ()=>setMatches(media.matches);
+        addEventListener("resize",listener)
+
+        return ()=> window.removeEventListener("resize",listener)
+    },[matches,query]) // we all know useEffect takes 2 argument 2n
+}
+
+export default useMediaQuery
--- 30-days react/PortFolio/tailwind.config.js
+++ 30-days react/PortFolio/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        blue: "#2cbce9",
+        red:"#dc4492",
+        yellow:"#fdcc49",
+        grey:"#ededed",
+        "deep-blue":"#010026",
+        "dark-grey":"#757575",
+        "opaque-black":"rgba(0,0,0,0.35)"
+      }
+    },
   },
   plugins: [],
-}
+};
