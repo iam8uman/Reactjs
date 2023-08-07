cac9e70f791b1346826bc96b705c01f863e3d111
iamsuman <suman.191544@ncit.edu.np>
2023-08-07 23:05:28 +0545

    Navbar page started

--- 30-days react/PortFolio/src/App.jsx
+++ 30-days react/PortFolio/src/App.jsx
@@ -3,12 +3,15 @@ import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
 
 export default function App() {
-  const [selectedpage, setSelectedpage] = useState("home");
+  const [selectedPage, setSelectedPage] = useState("home");
   const isAboveMediumScreen = useMediaQuery("(min-width:1060px)");
 
   return (
     <div className="app bg-deep-blue">
-      <Navbar />
+      <Navbar
+      selectedpage={selectedPage}
+      setSelectedpage={setSelectedPage}
+        />
     </div>
   );
 }
--- 30-days react/PortFolio/src/scenes/Navbar.jsx
+++ 30-days react/PortFolio/src/scenes/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import  { useState } from 'react'
+import useMediaQuery from '../hooks/useMediaQuery'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-const Navbar = () => {
+const Navbar = ({selectedPage,setSelectedPage}) => {
+    const [isMenuToggled,setIsMenuToggled]=useState(false)
+    const isAboveSmallScreen = useMediaQuery("(min-width:768px)");
+
   return (
-    <div>
+    <div className={`z-40 w-full fixed top-0 py-6`}>
       This Is navbar
     </div>
   )
