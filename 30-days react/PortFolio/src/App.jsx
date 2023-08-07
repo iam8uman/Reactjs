30966189c1db64045f451456a0714b6fe4264603
iamsuman <suman.191544@ncit.edu.np>
2023-08-07 22:52:28 +0545

    Navbar start vayo hai

--- 30-days react/PortFolio/src/App.jsx
+++ 30-days react/PortFolio/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react"
-import useMediaQuery from './hooks/useMediaQuery'
-
-
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import Navbar from "./scenes/Navbar";
 
 export default function App() {
-  const [selectedpage,setSelectedpage]=useState("home")
-  const isAboveMediumScreen=useMediaQuery("(min-width:1060px)")
-  
+  const [selectedpage, setSelectedpage] = useState("home");
+  const isAboveMediumScreen = useMediaQuery("(min-width:1060px)");
+
   return (
-    <div className="app bg-deep-blue"></div>
-  )
+    <div className="app bg-deep-blue">
+      <Navbar />
+    </div>
+  );
 }
