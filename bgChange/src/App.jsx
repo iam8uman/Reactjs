66cf102f4f2a46939463fcb4696ab5876dff093b
iamsuman <suman.191544@ncit.edu.np>
2023-10-09 09:04:50 +0545

    bgChange is complete useState+ tailwindcss

--- bgChange/src/App.jsx
+++ bgChange/src/App.jsx
@@ -1,12 +1,35 @@
-import Footerbar from "./components/Footerbar"
+// import Footerbar from "./components/Footerbar"
+import { useState } from "react";
+
 function App() {
-  // const [count, setCount] = useState(0)
+  const [color, setColor] = useState("red");
+
+  const bgChange = (c) => {
+    setColor(c);
+  };
 
   return (
     <>
-     <Footerbar />
+      {/* <Footerbar /> */}
+
+      <div className={`mainbody bg-${color}-700 h-screen w-screen`}>
+        <div className="btnList flex flex-wrap inset-x-10 fixed bottom-10 gap-10 justify-center bg-slate-400 p-5 rounded-lg">
+          <button className="bg-red-700" onClick={() => bgChange("red")}>
+            Red
+          </button>
+          <button className="bg-purple-700" onClick={() => bgChange("purple")}>
+            Purple
+          </button>
+          <button className="bg-green-700" onClick={() => bgChange("green")}>
+            Green
+          </button>
+          <button className="bg-yellow-700" onClick={() => bgChange("yellow")}>
+            Yellow
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- bgChange/src/components/Footerbar.jsx
+++ bgChange/src/components/Footerbar.jsx
@@ -1,25 +1,28 @@
-// import React from 'react'
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Footerbar = () => {
   const [color, setColor] = useState("white");
 
+  const bgChange = (color) => {
+    setColor(color);
+  };
+
   return (
     <>
-      <div className="mainbody " style={{backgroundColor:"red", height:"100vh"}}>
-        <div
-          className="bg-slate-600 h-100vh btn-wrapper m-5 p-5 fixed bottom-10 inset-x-9 flex flex-wrap justify-center"
-          style={{ backgroundColor: { color } }}
-        >
-          <button
-            className="bg-red-700 rounded-xl  mx-5"
-            onClick={() => setColor("red")}
-          >
+      <div className={`mainbody bg-${color}-700 h-96 w-96`} >
+        <div className="bg-slate-600  btn-wrapper m-5 p-5 fixed bottom-10 inset-x-9 flex flex-wrap justify-center">
+          <button className="bg-red-700 rounded-xl mx-5" onClick={() => bgChange("red")}>
             Red
           </button>
-          <button className="bg-purple-700 rounded-xl mx-5">Purple</button>
-          <button className="bg-green-700 rounded-xl mx-5">Green</button>
-          <button className="bg-blue-700 rounded-xl mx-5">Blue</button>
+          <button className="bg-purple-700 rounded-xl mx-5" onClick={() => bgChange("purple")}>
+            Purple
+          </button>
+          <button className="bg-green-700 rounded-xl mx-5" onClick={() => bgChange("green")}>
+            Green
+          </button>
+          <button className="bg-blue-700 rounded-xl mx-5" onClick={() => bgChange("blue")}>
+            Blue
+          </button>
         </div>
       </div>
     </>
