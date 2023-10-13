dda44442e5bbdc1743d554df05b12cdfe5388059
iamsuman <suman.191544@ncit.edu.np>
2023-10-13 23:17:27 +0545

    custom hoook for fetching data

--- customHook/src/App.tsx
+++ customHook/src/App.tsx
@@ -1,33 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="head">
+        <h1>Custom Hook</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque repellat libero eligendi, sequi facilis nisi harum aliquid molestiae provident rerum voluptate sapiente dolor? Eum tempore voluptas enim quisquam consectetur atque.</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+
     </>
   )
 }
