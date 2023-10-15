837639c1ea9697fb22261ad560e05f6b6959a21d
iamsuman <suman.191544@ncit.edu.np>
2023-10-15 08:03:40 +0545

    Upto Props validataion

--- currencyConverter/src/hooks/useCurrencyInfo.js
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,16 +1,18 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
-const useCurrencyInfo = () => {
+const useCurrencyInfo = (currency) => {
+  const [data, setData] = useState({});
 
-    const [data,setData]=useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((d) => setData(d[currency]));
+    console.log(data);
+    console.table(data);
+  }, [currency]);
+  return data;
+};
 
-    useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res)=>res.json())
-        .then((d)=>setData(d))
-
-    },[])
-
-}
-
-export default useCurrencyInfo
+export default useCurrencyInfo;
