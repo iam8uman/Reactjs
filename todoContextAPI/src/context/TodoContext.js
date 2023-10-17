9e5b328fe6e82e5f4f57dd640e89926041f0139f
iamsuman <suman.191544@ncit.edu.np>
2023-10-17 15:41:07 +0545

    todoContext API

---
+++ todoContextAPI/src/context/TodoContext.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+
+const TodoContext = React.createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Message of TODO",
+            completed:false
+        }
+    ],
+    addTodo:(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggoleComplete:(id)=>{}
+});
+
+export const useTodo=()=>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider=TodoContext.Provider;
---
+++ todoContextAPI/src/context/index.js
@@ -0,0 +1,2 @@
+// it is used for import export
+export {todoContext,useTodo,TodoProvider} from './TodoContext'
