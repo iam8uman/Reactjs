ea1ab95e1c4575bc8749c8ce86cfda4fe743ba4d
iamsuman <suman.191544@ncit.edu.np>
2023-10-18 10:03:20 +0545

    todo context api completed

--- todoContextAPI/src/context/TodoContext.js
+++ todoContextAPI/src/context/TodoContext.js
@@ -1,22 +1,22 @@
-import React, { useContext } from "react";
+import {createContext, useContext} from "react"
 
- export const TodoContext = React.createContext({
-    todos:[
+export const TodoContext = createContext({
+    todos: [
         {
-            id:1,
-            todo:"Message of TODO",
-            completed:false
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
         }
     ],
-    addTodo:(todo)=>{todo},
-    updateTodo:(id,todo)=>{id,todo},
-    deleteTodo:(id)=>{id},
-    toggleComplete:(id)=>{id}
-});
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
 
 
-export const useTodo=()=>{
+export const useTodo = () => {
     return useContext(TodoContext)
 }
 
-export const TodoProvider=TodoContext.Provider;
+export const TodoProvider = TodoContext.Provider
