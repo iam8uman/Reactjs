bbffe2d1f428a0c811a1646c9573e71df99c6b0d
iamsuman <suman.191544@ncit.edu.np>
2023-10-23 20:45:35 +0545

    reducers, state action and payload ko barema

--- ReduxToolkit/src/features/todo/todoSlice.js
+++ ReduxToolkit/src/features/todo/todoSlice.js
@@ -8,8 +8,18 @@ export const todoSlice = createSlice({
     name: "todo",
     initialState,
     reducers: {
-        addTodo:(state,action)=>{},
-        removeTodo:()=>{},
+        addTodo:(state,action)=>{
+            const todo = {
+                id: nanoid(),
+                text: action.payload,
+                completed: false,
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state,action)=>{
+            const idd = action.payload
+            state.todos = state.todos.filter((todo)=>todo.id!==idd)
+        },
         // Context API ma declare matra gartheu yeha define ne garnu parxa!
     }
 })
